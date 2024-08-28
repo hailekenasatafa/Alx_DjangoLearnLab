c3e744af59e4fba478ffdb69cc48811e301ee16d
obi <obi@obi.obi>
2024-08-28 04:52:44 -0400

    3

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from .forms import BookSearchForm
 
 def list_books(request):
     books = Book.objects.all()
@@ -13,3 +14,13 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+  
+
+def search_books(request):
+    form = BookSearchForm(request.GET or None)
+    books = Book.objects.all()
+    if form.is_valid():
+        query = form.cleaned_data.get('q')
+        if query:
+            books = books.filter(title__icontains=query)
+    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
