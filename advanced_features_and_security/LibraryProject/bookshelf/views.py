6d236b4bb3a7a766a995cc5f8038d81f61a99693
obi <obi@obi.obi>
2024-08-28 06:23:57 -0400

    1

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from .forms import BookSearchForm
 
 def list_books(request):
     books = Book.objects.all()
@@ -11,5 +12,12 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    context_object_name = 'library' 
 
+
+def form_example_view(request):
+    form = BookSearchForm(request.POST or None)
+    if form.is_valid():
+        # Handle the form submission (e.g., process data, save to database)
+        pass
+    return render(request, 'bookshelf/form_example.html', {'form': form})
--- advanced_features_and_security/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import form_example_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('form-example/', form_example_view, name='form_example'),
+    # Other URL patterns
 ]
+
+ 
+ 
+
+
+
