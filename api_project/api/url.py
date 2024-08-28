1c3a6604e2497f6063fa789fa65c3cd1427e1922
obi <obi@obi.obi>
2024-08-28 09:01:04 -0400

    1

--- api_project/api/url.py
+++ api_project/api/url.py
@@ -1,6 +1,31 @@
 from django.urls import path
 from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet 
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+]
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
