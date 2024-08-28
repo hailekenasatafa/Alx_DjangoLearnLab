109ab2e24b70a27a03f16f19dca25646ef295015
obi <obi@obi.obi>
2024-08-28 08:12:47 -0400

    1

--- advanced_features_and_security/LibraryProject/models.py
+++ advanced_features_and_security/LibraryProject/models.py
@@ -1,18 +1,7 @@
- 
 from django.contrib.auth.models import BaseUserManager
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    def __str__(self):
-        return self.username
-
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, date_of_birth=None, password=None, **extra_fields):
+    def create_user(self, username, email, date_of_birth, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
@@ -21,15 +10,8 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superuser(self, username, email, date_of_birth, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(username, email, password=password, **extra_fields)
-
- 
+        return self.create_user(username, email, date_of_birth, password, **extra_fields)
