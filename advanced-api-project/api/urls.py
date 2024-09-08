52157db5bb847feccc970eb4b2d1e4e860bebc4b
obi <obi@obi.obi>
2024-09-08 00:11:30 -0400

    1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 from .views import BookListCreateView, BookRetrieveUpdateDestroyView
+from .views import BookListCreateView, BookDetailView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail-update-delete'),
 ]
+
+
+ 
+
+ 
+
+ 
+
+ 
