ab55224cf7967a87a3b31ccfe6a8b84ab3114ac8
obi <obi@obi.obi>
2024-09-16 10:49:51 -0400

    1

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
+    "add_book/", "edit_book/", "delete_book"
 ]
 
  
