e0597610a84c9b5b3a317708d93b5ec358c53fd1
obi <obi@obi.obi>
2024-09-16 07:39:06 -0400

    1

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('api/', include('posts.urls')),
+    "unfollow/<int:user_id>/", "follow/<int:user_id>"
 ]
  
