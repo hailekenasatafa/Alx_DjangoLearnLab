e8cd870c5fa92d5bf76d44afb91789457c7a40e7
obi <obi@obi.obi>
2024-09-16 10:11:45 -0400

    1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -76,3 +76,4 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     
 
 "CommentCreateView", "CommentUpdateView", "CommentDeleteView"
+"Post.objects.filter", "title__icontains", "tags__name__icontains", "content__icontains"
